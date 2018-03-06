c15ce6947afdbb095de6b93425a8dbeda651b4b4
venedikttsulenev <venedikttsulenev@list.ru>
2018-03-06 15:03:17 +0300

    Zip command

---
+++ src/main/java/commands/UnzipCommand.java
@@ -0,0 +1,16 @@
+package commands;
+
+import utils.Environment;
+
+public class UnzipCommand extends Command {
+
+
+    private UnzipCommand() {
+        super("unzip", "unzip <file>", "decompress zip-archive");
+    }
+
+    @Override
+    public void execute(String[] args, Environment env) {
+        /* TODO */
+    }
+}
--- src/main/java/commands/ZipCommand.java
+++ src/main/java/commands/ZipCommand.java
@@ -2,21 +2,59 @@ package commands;
 
 import utils.Environment;
 
+import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-public class ZipCommand implements Command {
+public class ZipCommand extends Command {
 
     private static final ZipCommand instance = new ZipCommand();
+    private static final int MEGABYTE = 1048576;
+    private static final int BUFFER_SIZE = MEGABYTE;
 
     public static ZipCommand getInstance() {
         return instance;
     }
 
-    private ZipCommand() {}
+    private ZipCommand() {
+        super("zip", "zip <file1> <file2> ... <output file>", "create zip archive");
+    }
 
     @Override
     public void execute(String[] args, Environment env) {
         Path dir = env.getCurrentDirectory();
-        /* TODO */
+        Console console = env.getConsole();
+        if (args.length < 2)
+            console.printf("Too few arguments: at least 2 expected%n");
+        else {
+            File outputFile = Paths.get(dir.toString(), args[args.length - 1]).toFile();
+            try {
+                outputFile.createNewFile();
+                ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(outputFile));
+                FileInputStream fIn;
+                for (int i = 0; i < args.length - 1; ++i) {
+                    File file = Paths.get(dir.toString(), args[i]).toFile();
+                    if (!file.exists()) {
+                        console.printf("Error: file '%s' does not exist%n", args[i]);
+                    }
+                    else {
+                        fIn = new FileInputStream(file);
+                        ZipEntry entry = new ZipEntry(file.getName());
+                        zipOut.putNextEntry(entry);
+                        int bytesRead;
+                        byte[] buffer = new byte[BUFFER_SIZE];
+                        while (-1 != (bytesRead = fIn.read(buffer, 0, BUFFER_SIZE)))
+                            zipOut.write(buffer, 0, bytesRead);
+                        fIn.close();
+                        zipOut.closeEntry();
+                    }
+                }
+                zipOut.close();
+            } catch (IOException e) {
+                console.printf("Error: could create file '%s': %s%n", outputFile.getName(), e.getMessage());
+            }
+        }
     }
 }
