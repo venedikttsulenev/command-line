dddb84299de06f8fe1f15d9f71c1751ec3de31ef
venedikttsulenev <venedikttsulenev@list.ru>
2018-03-14 03:10:47 +0300

    Command parser improvement: Arguments containing whitespace characters can be put in double quotes now

--- src/main/java/utils/ParseUtils.java
+++ src/main/java/utils/ParseUtils.java
@@ -1,5 +1,9 @@
 package utils;
 
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ParseUtils {
     public static class ParseResult {
 
@@ -20,17 +24,32 @@ public class ParseUtils {
         }
     }
 
+    private static final Pattern argumentPattern = Pattern.compile("(?<quoted>\"[^\"]*\")|\\S+");
+
+    private static String[] splitArguments(String argsString) {
+        Matcher matcher = argumentPattern.matcher(argsString);
+        LinkedList<String> argsList = new LinkedList<>();
+        while (matcher.find()) {
+            String match = matcher.group();
+            if (matcher.group("quoted") != null)
+                argsList.add(match.substring(1, match.length() - 1));
+            else
+                argsList.add(match);
+        }
+        return argsList.toArray(new String[argsList.size()]);
+    }
+
     public static ParseResult parseCommand(String line) {
         String commandName = line;
         String[] arguments;
-        int spaceIndex = 0;
-        while (spaceIndex < line.length() && !Character.isWhitespace(line.charAt(spaceIndex)))
-            ++spaceIndex;
-        if (spaceIndex != line.length()) { /* There are some arguments */
-            commandName = line.substring(0, spaceIndex);
-            while (Character.isWhitespace(line.charAt(spaceIndex)))
-                ++spaceIndex;
-            arguments = line.substring(spaceIndex).split("\\s+");
+        int whitespaceIndex = 0;
+        while (whitespaceIndex < line.length() && !Character.isWhitespace(line.charAt(whitespaceIndex)))
+            ++whitespaceIndex;
+        if (whitespaceIndex != line.length()) { /* There are some arguments */
+            commandName = line.substring(0, whitespaceIndex);
+            while (Character.isWhitespace(line.charAt(whitespaceIndex)))
+                ++whitespaceIndex;
+            arguments = splitArguments(line.substring(whitespaceIndex));
         } else { /* There is no arguments */
             arguments = new String[0];
         }
